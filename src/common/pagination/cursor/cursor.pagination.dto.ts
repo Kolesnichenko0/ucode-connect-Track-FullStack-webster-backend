11b80e3ebc462ae01eee06af3d36364dab206ec7
mhrytsenko <maxgritsenko666@gmail.com>
2025-06-10 16:33:45 +0300

    Modify cursor pagination dto and types

--- src/common/pagination/cursor/cursor.pagination.dto.ts
+++ src/common/pagination/cursor/cursor.pagination.dto.ts
@@ -2,30 +2,16 @@
 import {
     IsCursorPaginationAfter,
     IsCursorPaginationLimit,
-    IsCursorType
-} from "./cursor.pagination.validator";
-import {ValidateNested} from "class-validator";
-import {Type} from "class-transformer";
-import {BaseCursor, CursorType, EventCursor} from "./cursor.pagination.types";
+    IsCursorType,
+} from './cursor.pagination.validator';
+import { ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { BaseCursor, CursorType } from './cursor.pagination.types';
 
-export class CursorPaginationDto {
-    @IsCursorType(false)
-    type: CursorType;
-
+export class CursorPaginationDto<T extends BaseCursor> {
     @IsCursorPaginationAfter(true)
     @ValidateNested()
-    @Type(options => {
-        if (options?.object && 'type' in options.object) {
-            switch (options.object.type) {
-                case CursorType.EVENT:
-                    return EventCursor;
-                default:
-                    return EventCursor;
-            }
-        }
-        return EventCursor;
-    })
-    after?: BaseCursor;
+    after?: T;
 
     @IsCursorPaginationLimit(true)
     limit: number = 10;
